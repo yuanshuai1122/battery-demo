2267092dccc49f64e6d80cb2f53c206da274eb58
shuaiyuan <shuaiyuan1122@gmail.com>
2023-11-06 22:31:28 +0800

    prepare

--- train.py
+++ train.py
@@ -2,6 +2,13 @@ import os
 import shutil
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error, r2_score
+from joblib import dump
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.python.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense
 
 
 # 带天数的字符串转秒 2-05:50:36
@@ -15,7 +22,7 @@ def str_to_seconds(time_str: str):
 
 # 删除某个文件或者某个文件夹下所有文件
 def delete_all_files_in_folder(folder_path):
-    print(f"开始删除某个文件夹/文件， folder_path:{folder_path}")
+    print(f"开始删除文件夹/文件， folder_path:{folder_path}")
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
         try:
@@ -25,7 +32,7 @@ def delete_all_files_in_folder(folder_path):
                 shutil.rmtree(file_path)  # 删除目录
         except Exception as e:
             print(f'Failed to delete {file_path}. Reason: {e}')
-    print(f"删除某个文件夹/文件完成， folder_path:{folder_path}")
+    print(f"删除文件夹/文件完成， folder_path:{folder_path}")
 
 
 # 文件处理
@@ -73,31 +80,36 @@ def split_file(file_with_path, split_path):
 
     # 按照指定的行索引拆分DataFrame并保存为Excel文件
     start = 0
-    last_part = pd.DataFrame()
-    for i in indices:
-        print(f"开始切分第{i}个文件.start:{start}")
+    for index, value in enumerate(indices):
+        print(f"开始切分第{index + 1}个文件.start:{start}")
         # 如果不是第一篇 需要跳过文字行
         if start != 0:
             start = start + 1
-        df_part = df.iloc[start:i]
+        df_part = df.iloc[start:value].copy()
         df_part['测试时间'] = pd.to_numeric(df_part['测试时间'].apply(str_to_seconds), errors='coerce')
         df_part['步骤时间'] = pd.to_numeric(df_part['步骤时间'].apply(str_to_seconds), errors='coerce')
-
+        # 删除测试时间列
+        df_part = df_part.drop('测试时间', axis=1)
         # 获取符合条件的最后一行的步骤时间
-        filtered_df_CCC = df_part[df_part['工步状态'] == 'CCC']
-        last_operate_time = filtered_df_CCC.iloc[-1]['步骤时间']
+        filtered_df_ccc = df_part[df_part['工步状态'] == 'CCC']
+        last_operate_time = filtered_df_ccc.iloc[-1]['步骤时间']
+        # 拷贝一列操作到 测试时间
+        df_part['测试时间'] = df_part['步骤时间'].copy()
         # 更新符合条件的某一列的值
-        filtered_df_CVC = df_part[df_part['工步状态'] == 'CVC']
-        filtered_df_CVC['步骤时间'] = filtered_df_CVC['步骤时间'] + last_operate_time
+        filtered_df_cvc = df_part[df_part['工步状态'] == 'CVC']
+        # 更新到测试时间
+        filtered_df_cvc['测试时间'] = filtered_df_cvc['测试时间'].copy() + last_operate_time
         # 将更新后的结果写入原始数据框
-        df_part.update(filtered_df_CVC)
+        df_part.update(filtered_df_cvc)
 
-        # # 将步骤时间列的值覆盖到测试时间列的值
-        # df['测试时间'] = df['步骤时间']
+        # 计算SOH到某SOH列
+        last_capacity = df_part['容量/Ah'].iloc[-1]
+        df_part['SOH'] = round(last_capacity / 30, 2)
 
-        df_part.to_excel(split_path + f'/split_{start}_{i}.xlsx', index=False)
-        start = i
-        last_part = df_part
+        # 写入到excel
+        df_part.to_excel(split_path + f'/第_{index + 1}_循环.xlsx', index=False)
+        start = value
+
     print(f'文件切分完毕，返回split_path:{split_path}')
     return split_path
 
@@ -106,8 +118,8 @@ def draw_img(folder_path, x_column, y_column):
     # 画图
     plt.figure(figsize=(10, 6))
 
-    filenames = os.listdir(folder_path)
     # 遍历文件夹中的所有 Excel 文件
+    filenames = os.listdir(folder_path)
     for filename in filenames[::500]:
     #for filename in filenames:
         if filename.endswith('.xlsx') or filename.endswith('.xls'):
@@ -125,15 +137,140 @@ def draw_img(folder_path, x_column, y_column):
 
     plt.show()
 
+def draw_img_soh(folder_path, x_column):
+
+    # y轴
+    y_column = 'SOH'
+    # 声明所有y值
+    y_values = list()
+    x_values = list()
+    # 遍历文件夹中的所有 Excel 文件
+    filenames = os.listdir(folder_path)
+    for filename in filenames[::500]:
+    #for filename in filenames:
+        if filename.endswith('.xlsx') or filename.endswith('.xls'):
+            print("SOH出图前开始处理：", filename)
+            # 读取 Excel 文件
+            df = pd.read_excel(os.path.join(folder_path, filename))
+            y_values.append(df['SOH'].iloc[-1])
+            x_values.append(filename.split('_')[1])
+
+    # 画图
+    plt.figure(figsize=(10, 6))
+    plt.plot(x_values, y_values)
+    # 中文显示
+    plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
+    plt.xlabel(x_column)
+    plt.ylabel(y_column)
+    plt.title('测试时间-SOH 曲线')
+
+    plt.show()
+
+
+# 数据合并
+def combined_data(folder_path, target_path):
+    # 定义一个空的DataFrame用于存储拼接后的数据
+    combined_data = pd.DataFrame()
+    # 遍历文件夹下的文件
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".xlsx") or filename.endswith(".xls"):
+            print('合并处理文件:', filename)
+            if filename.split('_')[1] == 1:
+                print('跳过不合理数据: ', filename)
+                continue
+            # 文件地址
+            file_path = os.path.join(folder_path, filename)
+            # 读取Excel文件内容
+            df = pd.read_excel(file_path)
+            # 将数据拼接到combined_data中
+            combined_data = pd.concat([combined_data, df], ignore_index=True)
+
+    # 将拼接后的数据保存到新的Excel文件中
+    combined_data.to_excel(target_path + "/train_data.xlsx", index=False)
+
+    return target_path
+
+
+# 训练模型
+def train_model(file_with_path):
+    # 读取Excel文件
+    print("开始读取excel数据集:", file_with_path)
+    df = pd.read_excel(file_with_path)  # 请将'your_file.xlsx'替换为你的Excel文件的路径
+    # 填充缺失值
+    # df.fillna(df.mean(), inplace=True)
+
+    # 选择需要的参数
+    features = df[['步骤时间', '电流/A', '电压/V', '辅助温度/℃', '容量/Ah']]
+    labels = df['SOH']  # 假设"SOH"是我们的目标变量
+
+    print("数据集处理完毕")
+
+    # 将数据分割成训练集和测试集
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+    # 标准化数据
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    # 将数据重塑为CNN模型所需的形状
+    X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
+    X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
+
+    # 创建模型
+    model = Sequential()
+    model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(X_train.shape[1], 1)))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Flatten())
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(1))
+
+    # 编译模型
+    model.compile(optimizer='adam', loss='mse')
+
+    # 添加EarlyStopping和ModelCheckpoint回调函数：EarlyStopping用于提前停止训练，ModelCheckpoint用于保存模型
+    callbacks = [
+        EarlyStopping(patience=10, restore_best_weights=True),
+        ModelCheckpoint('./models/model.h5', save_best_only=True)
+    ]
+
+    # 训练模型
+    model.fit(X_train, y_train, epochs=100, validation_split=0.2, callbacks=callbacks, verbose=0)
+
+    # 打印模型总结
+    model.summary()
+
+    # 评估模型
+    loss = model.evaluate(X_test, y_test, verbose=0)
+    print('Test loss:', loss)
+
+    # 查看其他评估指标
+    predictions = model.predict(X_test)
+    mae = mean_absolute_error(y_test, predictions)
+    r2 = r2_score(y_test, predictions)
+    print('Test MAE:', mae)
+    print('Test R^2:', r2)
+
+    # 保存模型
+    model.save('./models/save_model.h5')
+
+    # 保存scaler
+    dump(scaler, './scalers/scaler.joblib')
+
+
+
+
 
 if __name__ == '__main__':
-#    new_file = handle_files('data/3号电池.xlsx')
-#    path = split_file(new_file, 'data/split')
-#     draw_img('data/split', '步骤时间', '容量/Ah')
-#     draw_img('data/split', '步骤时间', '电流/A')
-#    draw_img('data/split', '步骤时间', '电压/V')
-    draw_img('data/split', '步骤时间', '辅助温度/℃')
-    #draw_img('data/split', '电流/A')
-    #print(get_file_last_line("data/split/split_0_34.xlsx"))
+    # new_file = handle_files('data/3号电池.xlsx')
+    # path = split_file(new_file, 'data/split')
+    # draw_img('data/split', '测试时间', '容量/Ah')
+    # draw_img('data/split', '测试时间', '电流/A')
+    # draw_img('data/split', '测试时间', '电压/V')
+    # draw_img('data/split', '测试时间', '辅助温度/℃')
+    # draw_img_soh('data/split', '测试时间', 'SOH')
+    #train_path = combined_data('data/split', 'data/train')
+    train_model('data/train/train_data.xlsx')
+
 
 
