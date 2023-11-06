a42c696c30f3c579835f28ba6d7c1ec1d4bd0524
yuanshuai <yuanshuai@gyyx.cn>
2023-11-06 10:07:19 +0800

    出图

--- train.py
+++ train.py
@@ -3,9 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def str_to_seconds(time_str):
-    h, m, s = map(int, time_str.split(':'))
-    return h*3600 + m*60 + s
+# def str_to_seconds(time_str):
+#     h, m, s = map(int, time_str.split(':'))
+#     return h*3600 + m*60 + s
+
+# 带天数的字符串转秒 2-05:50:36
+def str_to_seconds(time_str: str):
+    time = time_str.split(":")
+    if "-" in time[0]:
+        split_day = time[0].split("-")
+        return int(split_day[0]) * 86400 + int(split_day[1]) * 3600 + int(time[1]) * 60 + int(time[2])
+    else:
+        return int(time[0]) * 3600 + int(time[1]) * 60 + int(time[2])
 
 
 # 文件处理
@@ -26,7 +35,8 @@ def handle_files(file_with_path: str):
     # 只保留需要的数据
     df = df[['测试时间', '步骤时间', '电流/A', '容量/Ah', '电压/V', '辅助温度/℃', '工步状态']]
     # 保留符合条件的行
-    df = df[(df['工步状态'] == 'CCC') | (df['工步状态'] == 'CVC') | (df['工步状态'] == '工步状态')]
+    #df = df[(df['工步状态'] == 'CCC') | (df['工步状态'] == 'CVC') | (df['工步状态'] == '工步状态')]
+    df = df[(df['工步状态'] == 'CCC') | (df['工步状态'] == '工步状态')]
 
     # 处理新文件路径
     file_split = file_with_path.split('/')
@@ -62,12 +72,14 @@ def split_file(file_with_path, split_path):
     print(f'文件切分完毕，返回split_path:{split_path}')
     return split_path
 
-def draw_img(folder_path):
+# 画曲线图
+def draw_img(folder_path, y_column):
     # 初始化一个空的 DataFrame 来保存所有文件的数据
     all_data = pd.DataFrame()
 
+    filenames = os.listdir(folder_path)
     # 遍历文件夹中的所有 Excel 文件
-    for filename in os.listdir(folder_path):
+    for filename in filenames[::500]:
         if filename.endswith('.xlsx') or filename.endswith('.xls'):
             print("出图前开始处理：", filename)
             # 读取 Excel 文件
@@ -75,7 +87,7 @@ def draw_img(folder_path):
             # 将数据添加到 all_data DataFrame
             all_data = pd.concat([all_data, df])
 
-    #all_data['测试时间'] = pd.to_numeric(all_data['测试时间'].apply(str_to_seconds), errors='coerce')
+    all_data['测试时间'] = pd.to_numeric(all_data['测试时间'].apply(str_to_seconds), errors='coerce')
     all_data['步骤时间'] = pd.to_numeric(all_data['步骤时间'].apply(str_to_seconds), errors='coerce')
     all_data['电流/A'] = pd.to_numeric(all_data['电流/A'], errors='coerce')
     all_data['容量/Ah'] = pd.to_numeric(all_data['容量/Ah'], errors='coerce')
@@ -87,19 +99,19 @@ def draw_img(folder_path):
 
     # 指定横纵坐标为 Excel 的列
     x_column = '测试时间'
-    y_column = '容量/Ah'
 
     # 中文显示
-    plt.rcParams['font.sans-serif'] = ['KaiTi', 'SimHei', 'FangSong']  # 汉字字体,优先使用楷体，如果找不到楷体，则使用黑体
-    plt.rcParams['font.size'] = 12  # 字体大小
-    plt.rcParams['axes.unicode_minus'] = False  # 正常显示负号
+    plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
+    # plt.rcParams['font.sans-serif'] = ['KaiTi', 'SimHei', 'FangSong']  # 汉字字体,优先使用楷体，如果找不到楷体，则使用黑体
+    # plt.rcParams['font.size'] = 12  # 字体大小
+    # plt.rcParams['axes.unicode_minus'] = False  # 正常显示负号
     # 画图
     plt.figure(figsize=(10, 6))
     plt.plot(all_data[x_column], all_data[y_column])
 
     plt.xlabel(x_column)
     plt.ylabel(y_column)
-    plt.title('时间-电流曲线')
+    plt.title(x_column + y_column + "曲线")
 
     plt.show()
 
@@ -108,6 +120,6 @@ if __name__ == '__main__':
     # new_file = handle_files('data/3号电池.xlsx')
     # path = split_file(new_file, 'data/split')
     # draw_img(path)
-    draw_img('data/split')
+    draw_img('data/split', '辅助温度/℃')
 
 
